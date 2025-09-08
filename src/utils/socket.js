2e9ab2993c19cfcf2aba798e4c5d62c7310a06e3
desizntech <desizntechcompany@gmail.com>
2025-09-08 16:20:30 +0530

    file name change

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,13 +1,13 @@
 import { io } from "socket.io-client";
 import { BaseUrl } from "./constance";
+
 export const createSocketConnection = () => {
-  //   const socket = io(BaseUrl, {
-  //     transports: ["websocket"],
-  //     withCredentials: true,
-  //   });
   if (location.hostname === "localhost") {
-    return io(BaseUrl, { transports: ["websocket"] });
+    return io(BaseUrl, { transports: ["websocket", "polling"] });
   } else {
-    return io("/", { path: "/api/socket.io", transports: ["websocket"] });
+    return io("/", {
+      path: "/socket.io",
+      transports: ["websocket", "polling"],
+    });
   }
 };
